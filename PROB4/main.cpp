4c72727d303a0856698f9af2d5798752095ae9d7
samyroyale <santimujim@gmail.com>
2021-12-16 22:48:58 -0500

    entregable

--- PROB4/main.cpp
+++ PROB4/main.cpp
@@ -1,49 +1,28 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
 
-int main()
-{
-    int hora1=0,hora2=0,minutos1=0,minutos2=0,minutosTot=0;
-
-    cout<<"Ingrese la primera hora:"<<endl;
-    cin>>hora1;
-    //se toma el numero de minutos y se verifica que este bien
-    minutos1= hora1-(hora1/100)*100;
-    if(minutos1<0 or minutos1>=60){
-        cout<<"El dato ingresado "<<minutos1<<" es invalido"<<endl;
-        return 0;
-    }
-    hora1/=100;
-    if(hora1<0 or hora1>24) {
-        cout<<"El dato ingresado "<<hora1<<" es invalido"<<endl;
-        return 0;
-    }
-
-    cout<<"Ingrese la segunda hora:"<<endl;
-    cin>>hora2;
-    //se toma el numero de minutos y se verifica que este bien
-    minutos2 = hora2-(hora2/100)*100;
-    if(minutos2<0 or minutos2>=60) {
-        cout<<"El dato ingresado "<<minutos2<<" es invalido"<<endl;
-        return 0;
-    }
-    hora2/=100;
-    if(hora2<0 or hora2>24) {
-        cout<<"El dato ingresado "<<hora2<<" es invalido"<<endl;
-        return 0;
-    }
-    minutosTot = minutos1 + minutos2;
-    //se cuenta el numero de minutos y se pasa a horas
-    for(;minutosTot>=60;minutosTot-=60)
-        hora1++;
-
-    if(hora1+hora2>24){
-        cout<<"La suma de horas es "<<hora1+hora2<<" y es mayor a 24"<<endl;
-        return 0;
-    }
-    else if(minutosTot<10) cout<<"La hora es:"<<(hora1+hora2)*10<<minutosTot<<endl;
-    else cout<<"La hora es:"<<hora1+hora2<<minutosTot<<endl;
-
+int main() {
+     int a, b, c, d, N, n;
+             cout << " Ingrese el primero numero: " ;
+             cin >> N;
+             cout << " Ingrese el segundo numero: " ;
+             cin >> n;
+             a = N / 100 ;
+             b = N- (a * 100 );
+             c = n / 100 ;
+             d = n- (c * 100 );
+             if (a> 24 || b> 59 || c> 24 || d> 59 ) {
+                 cout << " uno de los datos ingresados ​​es invalido " << endl;
+             }
+             else {
+             a = a + c;
+             b = b + d;
+             if (b> 59 ) {
+                 a = a + 1 ;
+                 b = 60 -b;
+             }
+             b = b * - 1 ;
+             cout << " La hora es: " << a << b << endl;
+             }
     return 0;
 }
